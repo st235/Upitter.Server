337329f97fea8ebe6bfa2779a55f7207d0a9cb54
Alexadner Dadukin <st235@yandex.ru>
2016-06-27 23:47:38 +0300

    Созданы категории

--- config/categories.js
+++ config/categories.js
@@ -12,5 +12,587 @@ module.exports = {
 	102: {
 		title: 'Бильярдные клубы',
 		parentCategory: 100
+	},
+	103: {
+		title: 'Ночные клубы',
+		parentCategory: 100
+	},
+	104: {
+		title: 'Пабы, пивные рестораны',
+		parentCategory: 100
+	},
+	105: {
+		title: 'Рок клубы',
+		parentCategory: 100
+	},
+	106: {
+		title: 'Танцевальные клубы',
+		parentCategory: 100
+	},
+	107: {
+		title: 'Караоке клубы',
+		parentCategory: 100
+	},
+	200: {
+		title: 'Магазины',
+	},
+	201: {
+		title: 'Предметы интерьера',
+		parentCategory: 200
+	},
+	202: {
+		title: 'Книжные магазины',
+		parentCategory: 200
+	},
+	203: {
+		title: 'Магазины детских игрушек',
+		parentCategory: 200
+	},
+	204: {
+		title: 'Магазины детской одежды',
+		parentCategory: 200
+	},
+	205: {
+		title: 'Магазины для беременных',
+		parentCategory: 200
+	},
+	206: {
+		title: 'Магазины ковров',
+		parentCategory: 200
+	},
+	207: {
+		title: 'Магазины компьютеров',
+		parentCategory: 200
+	},
+	208: {
+		title: 'Магазины косметики и парфюма',
+		parentCategory: 200
+	},
+	209: {
+		title: 'Магазины мебели',
+		parentCategory: 200
+	},
+	210: {
+		title: 'Магазины музыкальных инструментов',
+		parentCategory: 200
+	},
+	211: {
+		title: 'Магазины нижнего белья',
+		parentCategory: 200
+	},
+	212: {
+		title: 'Магазины обуви',
+		parentCategory: 200
+	},
+	213: {
+		title: 'Магазины одежды',
+		parentCategory: 200
+	},
+	214: {
+		title: 'Магазины подарков',
+		parentCategory: 200
+	},
+	215: {
+		title: 'Магазины посуды',
+		parentCategory: 200
+	},
+	216: {
+		title: 'Магазины света',
+		parentCategory: 200
+	},
+	217: {
+		title: 'Магазины сумок',
+		parentCategory: 200
+	},
+	218: {
+		title: 'Магазины Ткани',
+		parentCategory: 200
+	},
+	219: {
+		title: 'Магазины фототоваров',
+		parentCategory: 200
+	},
+	220: {
+		title: 'Магазины часов',
+		parentCategory: 200
+	},
+	221: {
+		title: 'Магазины электроники и бытовой техники',
+		parentCategory: 200
+	},
+	222: {
+		title: 'Магазины для дома и дачи',
+		parentCategory: 200
+	},
+	223: {
+		title: 'Магазины для взрослых',
+		parentCategory: 200
+	},
+	224: {
+		title: 'Торговые клмплексы',
+		parentCategory: 200
+	},
+	225: {
+		title: 'Ювелирные магазины',
+		parentCategory: 200
+	},
+	226: {
+		title: 'Спортивные магазины',
+		parentCategory: 200
+	},
+	300: {
+		title: 'Рестораны и кафе'
+	},
+	301: {
+		title: 'рестораны',
+		parentCategory: 300
+	},
+	302: {
+		title: 'Антикафе и коворкинг',
+		parentCategory: 300
+	},
+	303: {
+		title: 'Вегетарианские рестораны',
+		parentCategory: 300
+	},
+	304: {
+		title: 'Детские кафе',
+		parentCategory: 300
+	},
+	305: {
+		title: 'Закусочные',
+		parentCategory: 300
+	},
+	306: {
+		title: 'Кофейни',
+		parentCategory: 300
+	},
+	307: {
+		title: 'Пиццерии',
+		parentCategory: 300
+	},
+	308: {
+		title: 'Кальянные',
+		parentCategory: 300
+	},
+	309: {
+		title: 'Чайные',
+		parentCategory: 300
+	},
+	310: {
+		title: 'Японские рестораны',
+		parentCategory: 300
+	},
+	311: {
+		title: 'Кафе',
+		parentCategory: 300
+	},
+	400: {
+		title: 'Развлечения, отдых'
+	},
+	401: {
+		title: 'Цирки',
+		parentCategory: 400
+	},
+	402: {
+		title: 'Парки отдыха',
+		parentCategory: 400
+	},
+	403: {
+		title: 'Кинотеатры',
+		parentCategory: 400
+	},
+	404: {
+		title: 'Зоопарки',
+		parentCategory: 400
+	},
+	405: {
+		title: 'Боулинг',
+		parentCategory: 400
+	},
+	406: {
+		title: 'Бани,сауны',
+		parentCategory: 400
+	},
+	407: {
+		title: 'аттракционы',
+		parentCategory: 400
+	},
+	408: {
+		title: 'Аквапарки',
+		parentCategory: 400
+	},
+	409: {
+		title: 'Дельфинарии и океанариум',
+		parentCategory: 400
+	},
+	410: {
+		title: 'Концертные залы',
+		parentCategory: 400
+	},
+	411: {
+		title: 'Пляжи',
+		parentCategory: 400
+	},
+	412: {
+		title: 'Танцевальные школы',
+		parentCategory: 400
+	},
+	500: {
+		title: 'Спорт и акивный отдых'
+	},
+	501: {
+		title: 'Бассейны',
+		parentCategory: 500
+	},
+	502: {
+		title: 'Боевые искусства',
+		parentCategory: 500
+	},
+	503: {
+		title: 'Верховая езда',
+		parentCategory: 500
+	},
+	504: {
+		title: 'Гольф клубы',
+		parentCategory: 500
+	},
+	505: {
+		title: 'Горнолыжные склоны',
+		parentCategory: 500
+	},
+	506: {
+		title: 'Йога центры',
+		parentCategory: 500
+	},
+	507: {
+		title: 'Фитнес клубы',
+		parentCategory: 500
+	},
+	508: {
+		title: 'Яхт клубы',
+		parentCategory: 500
+	},
+	509: {
+		title: 'Прокат велосипедов',
+		parentCategory: 500
+	},
+	510: {
+		title: 'Спортивные секции',
+		parentCategory: 500
+	},
+	511: {
+		title: 'Стадионы',
+		parentCategory: 500
+	},
+	512: {
+		title: 'Прыжки с парашютом',
+		parentCategory: 500
+	},
+	513: {
+		title: 'Пейнтбол',
+		parentCategory: 500
+	},
+	514: {
+		title: 'Катки ледовые',
+		parentCategory: 500
+	},
+	515: {
+		title: 'Картинг',
+		parentCategory: 500
+	},
+	600: {
+		title: 'Ремонтные сервисы'
+	},
+	601: {
+		title: 'Компьютерные сервисы',
+		parentCategory: 600
+	},
+	602: {
+		title: 'Ремонт телефонов',
+		parentCategory: 600
+	},
+	603: {
+		title: 'Ремонт бытовой техники',
+		parentCategory: 600
+	},
+	604: {
+		title: 'Ремонт музыкальных инструментов',
+		parentCategory: 600
+	},
+	605: {
+		title: 'Ремонт обуви',
+		parentCategory: 600
+	},
+	606: {
+		title: 'Ремонт и обслуживание сантехники',
+		parentCategory: 600
+	},
+	607: {
+		title: 'Ремонт принтеров, заправка катреджей',
+		parentCategory: 600
+	},
+	608: {
+		title: 'Ремонт Телевизоров',
+		parentCategory: 600
+	},
+	609: {
+		title: 'Ремонт фотоаппаратов',
+		parentCategory: 600
+	},
+	610: {
+		title: 'Ремонт часов',
+		parentCategory: 600
+	},
+	611: {
+		title: 'Реставрация мебели',
+		parentCategory: 600
+	},
+	700: {
+		title: 'Авто'
+	},
+	701: {
+		title: 'Автозапчасти, аксессуары',
+		parentCategory: 700
+	},
+	702: {
+		title: 'Автомойки',
+		parentCategory: 700
+	},
+	703: {
+		title: 'Автосалоны',
+		parentCategory: 700
+	},
+	704: {
+		title: 'Автосервис, тюнинг',
+		parentCategory: 700
+	},
+	705: {
+		title: 'Автошколы',
+		parentCategory: 700
+	},
+	706: {
+		title: 'Аренда автомобилий',
+		parentCategory: 700
+	},
+	707: {
+		title: 'Шиномонтаж',
+		parentCategory: 700
+	},
+	708: {
+		title: 'Шины и диски',
+		parentCategory: 700
+	},
+	709: {
+		title: 'Эвакуаторы',
+		parentCategory: 700
+	},
+	800: {
+		title: 'Юридические услуги'
+	},
+	801: {
+		title: 'Адвокаты, юридические услуги',
+		parentCategory: 800
+	},
+	802: {
+		title: 'Натариус',
+		parentCategory: 800
+	},
+	900: {
+		title: 'Красота и здоровье'
+	},
+	901: {
+		title: 'Другие услуги',
+		parentCategory: 900
+	},
+	902: {
+		title: 'Spa',
+		parentCategory: 900
+	},
+	903: {
+		title: 'Стилисты',
+		parentCategory: 900
+	},
+	904: {
+		title: 'Маникюр салоны',
+		parentCategory: 900
+	},
+	905: {
+		title: 'Массаж',
+		parentCategory: 900
+	},
+	906: {
+		title: 'Парикмахерские',
+		parentCategory: 900
+	},
+	907: {
+		title: 'Пирсинг',
+		parentCategory: 900
+	},
+	908: {
+		title: 'Салоны красоты',
+		parentCategory: 900
+	},
+	909: {
+		title: 'Солярии',
+		parentCategory: 900
+	},
+	910: {
+		title: 'Тату салоны и пирсинг',
+		parentCategory: 900
+	},
+	1000: {
+		title: 'Растения,цветы'
+	},
+	1001: {
+		title: 'Магазины цветов',
+		parentCategory: 1000
+	},
+	1002: {
+		title: 'Садовые центры',
+		parentCategory: 1000
+	},
+	1100: {
+		title: 'Животные'
+	},
+	1101: {
+		title: 'Ветеринарные аптеки',
+		parentCategory: 1100
+	},
+	1102: {
+		title: 'Ветеринарные клиники',
+		parentCategory: 1100
+	},
+	1103: {
+		title: 'Зоомагазины',
+		parentCategory: 1100
+	},
+	1104: {
+		title: 'Питомники',
+		parentCategory: 1100
+	},
+	1105: {
+		title: 'Приюты для животных',
+		parentCategory: 1100
+	},
+	1106: {
+		title: 'Салоны для животных',
+		parentCategory: 1100
+	},
+	1200: {
+		title: 'Культура и искуство'
+	},
+	1201: {
+		title: 'музеи',
+		parentCategory: 1200
+	},
+	1202: {
+		title: 'Театры',
+		parentCategory: 1200
+	},
+	1203: {
+		title: 'Библиотеки',
+		parentCategory: 1200
+	},
+	1204: {
+		title: 'Концерты',
+		parentCategory: 1200
+	},
+	1205: {
+		title: 'Кино',
+		parentCategory: 1200
+	},
+	1206: {
+		title: 'Музыка',
+		parentCategory: 1200
+	},
+	1300: {
+		title: 'Медицина'
+	},
+	1301: {
+		title: 'Аптеки',
+		parentCategory: 1300
+	},
+	1302: {
+		title: 'Стоматология',
+		parentCategory: 1300
+	},
+	1303: {
+		title: 'Психологи',
+		parentCategory: 1300
+	},
+	1304: {
+		title: 'Медицинское оборудование',
+		parentCategory: 1300
+	},
+	1305: {
+		title: 'Частныее медицинские клиникие',
+		parentCategory: 1300
+	},
+	1306: {
+		title: 'Диагностический центр',
+		parentCategory: 1300
+	},
+	1307: {
+		title: 'Поликлиники',
+		parentCategory: 1300
+	},
+	1308: {
+		title: 'Больницы',
+		parentCategory: 1300
+	},
+	1309: {
+		title: 'Восточная медицина',
+		parentCategory: 1300
+	},
+	1400: {
+		title: 'Образование'
+	},
+	1401: {
+		title: 'Вузы',
+		parentCategory: 1400
+	},
+	1402: {
+		title: 'Колледжи',
+		parentCategory: 1400
+	},
+	1403: {
+		title: 'Курсы',
+		parentCategory: 1400
+	},
+	1404: {
+		title: 'Музыкальные школы',
+		parentCategory: 1400
+	},
+	1405: {
+		title: 'Тренинги и семинары',
+		parentCategory: 1400
+	},
+	1406: {
+		title: 'Художественные школы',
+		parentCategory: 1400
+	},
+	1407: {
+		title: 'Школы',
+		parentCategory: 1400
+	},
+	1500: {
+		title: 'Туризм'
+	},
+	1501: {
+		title: 'Визовые центры',
+		parentCategory: 1500
+	},
+	1502: {
+		title: 'Базы отдыха',
+		parentCategory: 1500
+	},
+	1503: {
+		title: 'Туроператоры',
+		parentCategory: 1500
+	},
+	1504: {
+		title: 'Турфирмы',
+		parentCategory: 1500
 	}
 };
