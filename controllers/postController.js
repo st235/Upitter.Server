ebdf3ef498ca365f5e7a3491f5baf8017cd40d6b
alexander <st235@yandex.ru>
2016-07-27 14:51:42 +0300

    Сорвиголова фиксы #1

--- controllers/postController.js
+++ controllers/postController.js
@@ -118,13 +118,13 @@ class PostsController extends BaseController {
 
 		if (req.query.limit) {
 			try {
-				req.query.limit = JSON.parse(req.query.limit); //   FIXME: seriously?)
+				req.query.limit = parseInt(req.query.limit, 10);
 			} catch (e) {
 				req.query.limit = 20;
 			}
 		}
 
-		const { latitude, longitude, radius = 0, limit, category, activity } = req.query;
+		const { latitude, longitude, radius = 0, limit = 20, category, activity } = req.query;
 
 		this
 			.postsManager
