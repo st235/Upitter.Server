fbd3e1f8ca2d87e6d2b4094020c4bca5497c56af
alexander <st235@yandex.ru>
2016-07-27 14:02:35 +0300

    Фиксы работы создания постов

--- models/response/postResponseModel.js
+++ models/response/postResponseModel.js
@@ -7,52 +7,50 @@ const CompanyResponseModel = require('./companyResponseModel');
 //TODO: Разобраться, почему CompanyResponseModel валит сервере
 
 module.exports = (userId, post, lang = 'en') => {
-	try {
-		const likedByMe = !!_.find(post.likeVoters, voterId => voterId === userId);
-		const votedByMe = !!_.find(post.votersForVariants, voterId => voterId === userId);
+	const likedByMe = !!_.find(post.likeVoters, voterId => voterId === userId);
+	const votedByMe = !!_.find(post.votersForVariants, voterId => voterId === userId);
 
-		const postResponse = {
-			customId: post.customId,
-			author: post.author,
-			title: post.title,
-			text: post.text,
-			category: post.category,
-			coordinates: {
-				latitude: post.location[0],
-				longitude: post.location[1]
-			},
-			fromNow: moment(post.createdDate).locale(lang).fromNow(),
-			likesAmount: post.likes,
-			commentsAmount: post.comments.length,
-			votersAmount: post.votersForVariants.length,
-			isLikedByMe: likedByMe,
-			isVotedByMe: votedByMe,
-			watchesAmount: post.watches
-		};
+	const postResponse = {
+		customId: post.customId,
+		author: post.author,
+		title: post.title,
+		text: post.text,
+		category: post.category,
+		coordinates: {
+			latitude: post.location[0],
+			longitude: post.location[1]
+		},
+		fromNow: moment(post.createdDate).locale(lang).fromNow(),
+		likesAmount: post.likes,
+		commentsAmount: post.comments.length,
+		votersAmount: post.votersForVariants.length,
+		isLikedByMe: likedByMe,
+		isVotedByMe: votedByMe,
+		watchesAmount: post.watches
+	};
 
-		if (post.comments.length > 0) postResponse.comments = post.comments;
-		if (post.variants.length > 0) {
-			postResponse.variants = _.map(post.variants, variant => {
-				let currentVoted = false;
-				if (votedByMe)
-					currentVoted = !!_.find(variant.voters, voterId => voterId === userId);
+	if (post.comments.length > 0) postResponse.comments = post.comments;
+	if (post.variants.length > 0) {
+		postResponse.variants = _.map(post.variants, variant => {
+			let currentVoted = false;
+			if (votedByMe)
+				currentVoted = !!_.find(variant.voters, voterId => voterId === userId);
 
-				const data = {
-					value: variant.value,
-					count: variant.count,
-					voters: variant.voters
-				};
+			const data = {
+				value: variant.value,
+				count: variant.count,
+				voters: variant.voters
+			};
 
-				if (currentVoted) data.myVote = true;
-				return data;
-			});
-		}
+			if (currentVoted) data.myVote = true;
+			return data;
+		});
+	}
 
-		if (post.likeVoters.length > 0) postResponse.likeVoters = post.likeVoters;
-		if (post.logoUrl) postResponse.logoUrl = post.logoUrl;
-		if (post.updatedDate) postResponse.updatedDate = post.updatedDate;
-		if (post.media && post.media.length) postResponse.media = post.media;
+	if (post.likeVoters.length > 0) postResponse.likeVoters = post.likeVoters;
+	if (post.logoUrl) postResponse.logoUrl = post.logoUrl;
+	if (post.updatedDate) postResponse.updatedDate = post.updatedDate;
+	if (post.media && post.media.length) postResponse.media = post.media;
 
-		return postResponse;
-	} catch (e) { console.log(e); }
+	return postResponse;
 };
