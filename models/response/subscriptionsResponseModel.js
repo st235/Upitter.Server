134e3087c6e328c3e8354182535126458e77ac2a
pinkeleph <navbior@gmail.com>
2016-09-20 21:34:01 +0300

    user subs fix

--- models/response/subscriptionsResponseModel.js
+++ models/response/subscriptionsResponseModel.js
@@ -5,7 +5,7 @@ const companyResponse = require('./companyResponseModel');
 
 module.exports = (user, limit, companyId) => {
 	if (user.subscriptions && user.subscriptions.length > 0 && user.subscriptions[0].customId) {
- -		let index = 0;
+		let index = 0;
 		if (companyId) _.each(user.subscriptions, (company, i) => (company.customId === companyId) ? index = i + 1 : index);
 		const subscriptions = user.subscriptions.splice(index, limit);
 		return _.map(subscriptions, subscription => {
@@ -14,7 +14,7 @@ module.exports = (user, limit, companyId) => {
 				alias: subscription.alias,
 				name: subscription.name,
 				logoUrl: subscription.logoUrl
-			}
+			};
 		});
 	} else {
 		return [];
