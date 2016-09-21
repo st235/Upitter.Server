0810b472b86ab04c30721d32b1b80c4234db9597
Zamyckov Dmitriy <navbior@gmail.com>
2016-09-21 15:24:40 +0300

    subscribersAmount фикс

--- models/response/companyResponseModel.js
+++ models/response/companyResponseModel.js
@@ -27,7 +27,7 @@ module.exports = company => {
 	if (company.accessToken) companyResponse.accessToken = company.accessToken;
 	if (company.socialLinks) companyResponse.socialLinks = company.socialLinks;
 	if (company.subscribers && company.subscribers.length && company.subscribers[0].customId) {
-		companyResponse.subscribersAmount = 0;
+		companyResponse.subscribersAmount = "0";
 		if (company.subscribers.length < 1000) companyResponse.subscribersAmount = company.subscribers.length.toString();
 		else if (company.subscribers.length < 1000000) companyResponse.subscribersAmount = Math.round(company.subscribers.length / 1000) + "k";
 		else companyResponse.subscribersAmount = Math.round(company.subscribers.length / 1000000) + "M";
