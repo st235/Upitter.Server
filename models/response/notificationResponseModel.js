5ad829a6d4fec0bc4132c627c89dd4114a4c3fe8
pinkeleph <navbior@gmail.com>
2016-11-29 18:38:58 +0300

    фиксы уведомлений

--- models/response/notificationResponseModel.js
+++ models/response/notificationResponseModel.js
@@ -1,5 +1,7 @@
 'use strict';
 const LocaleService = require('default-locale');
+const userResponseModel = require('./userResponseModel');
+const companyResponseModel = require('./companyResponseModel');
 
 module.exports = (notification, language) => {
 	const notificationResponse = {
@@ -10,8 +12,14 @@ module.exports = (notification, language) => {
 		createdDate: notification.createdDate
 	};
 
-	if (notification.authorCompany) notificationResponse.text = `${notification.authorCompany.name} ${LocaleService.getString(`${notification.type}`, language)}`;
-	if (notification.authorUser) notificationResponse.text = `${notification.authorUser.nickname} ${LocaleService.getString(`${notification.type}`, language)}`;
+	if (notification.authorCompany) {
+		notificationResponse.company = companyResponseModel(notification.authorCompany);
+		notificationResponse.text = `${notification.authorCompany.name} ${LocaleService.getString(`${notification.type}`, language)}`;
+	}
+	if (notification.authorUser) {
+		notificationResponse.user = userResponseModel(notification.authorUser);
+		notificationResponse.text = `${notification.authorUser.nickname} ${LocaleService.getString(`${notification.type}`, language)}`;
+	}
 	if (notification.type === 'like' || notification.type === 'post') notificationResponse.postId = notification.targetId;
 
 	return notificationResponse;
